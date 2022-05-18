ba169ac9dfd9c1749e9ce153878aab157831f939
Telthem89 <telthem.kerlyeen@gmail.com>
2022-05-18 15:45:37 +0200

    App portal

---
+++ src/test/java/com/sagehill/sagehill/SagehillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagehill.sagehill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagehillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
